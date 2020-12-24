3849a3ea6ca0c88940fe4f6b3decb3286e9cee17
hectoralarcon <hectoralarconguerra@gmail.com>
2020-12-24 15:06:39 -0500

    Utilizando diseños ya establecidos.

--- App.js
+++ App.js
@@ -12,19 +12,55 @@ import Inicio from './views/Inicio';
 import NuevoCliente from './views/NuevoCliente';
 import DetallesCliente from './views/DetallesCliente';
 
+import {DefaultTheme, Provider as PaperProvider} from 'react-native-paper';
+
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 
 const Stack = createStackNavigator();
 
+//Definir el tema
+const theme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: '#1774F2',
+    accent: '#0655BF',
+  },
+};
+
+//console.log(theme.colors);
+
 const App = () => {
   return (
     <>
       <NavigationContainer>
-        <Stack.Navigator initialRouteName="NuevoCliente">
+        <Stack.Navigator
+          initialRouteName="Inicio"
+          screenOptions={{
+            headerStyle: {
+              backgroundColor: theme.colors.primary,
+            },
+            headerTintColor: theme.colors.surface,
+            headerTitleStyle: {
+              fontWeight: 'bold',
+            },
+          }}>
           <Stack.Screen name="Inicio" component={Inicio} />
-          <Stack.Screen name="NuevoCliente" component={NuevoCliente} />
-          <Stack.Screen name="DetallesCliente" component={DetallesCliente} />
+          <Stack.Screen
+            name="NuevoCliente"
+            component={NuevoCliente}
+            options={{
+              title: 'Nuevo Cliente',
+            }}
+          />
+          <Stack.Screen
+            name="DetallesCliente"
+            component={DetallesCliente}
+            options={{
+              title: 'Detalles Cliente',
+            }}
+          />
         </Stack.Navigator>
       </NavigationContainer>
     </>
