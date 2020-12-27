147fd351e3654b2933593372f3f5cdb982919782
hectoralarcon <hectoralarconguerra@gmail.com>
2020-12-26 22:35:29 -0500

    barra

---
+++ components/ui/Barra.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import {Button} from 'react-native-paper';
+
+const BarraSuperior = () => {
+  return <Button onPress={() => handlePress()}>Cliente</Button>;
+};
+
+export default BarraSuperior;
